197b956a776029a45e630ce0276218f9c8a3cd47
nsthakur <nsthakur@mit.edu>
2023-04-14 10:18:38 -0400

    working min dist function for single segment

--- src/pure_pursuit.py
+++ src/pure_pursuit.py
@@ -6,19 +6,20 @@ import time
 import utils
 import tf
 
-from geometry_msgs.msg import PoseArray, PoseStamped
+from geometry_msgs.msg import PoseArray, PoseStamped, PointStamped, Point
 from visualization_msgs.msg import Marker
 from ackermann_msgs.msg import AckermannDriveStamped
 from nav_msgs.msg import Odometry
+from std_msgs.msg import Header
 
 class PurePursuit(object):
     """ Implements Pure Pursuit trajectory tracking with a fixed lookahead and speed.
     """
     def __init__(self):
-        self.odom_topic       = rospy.get_param("~odom_topic")
-        self.lookahead        = # FILL IN #
+        self.odom_topic       = rospy.get_param("~odom_topic", "/pf/pose/odom")
+        # self.lookahead        = # FILL IN #
         self.speed            = 2
-        self.wheelbase_length = # FILL IN #
+        # self.wheelbase_length = # FILL IN #
         
         self.pose = None
 
@@ -26,6 +27,8 @@ class PurePursuit(object):
         self.traj_sub = rospy.Subscriber("/trajectory/current", PoseArray, self.trajectory_callback, queue_size=1)
         self.pose_sub = rospy.Subscriber(self.odom_topic, Odometry, self.pose_callback, queue_size=1)
         self.drive_pub = rospy.Publisher("/drive", AckermannDriveStamped, queue_size=1)
+
+        self.min_dist_pub = rospy.Publisher("/min_point", PointStamped, queue_size=1)
 
     def trajectory_callback(self, msg):
         ''' Clears the currently followed trajectory, and loads the new one from the message
@@ -37,19 +40,28 @@ class PurePursuit(object):
 
     def pose_callback(self, odom):
         self.pose = odom.pose.pose
+        min_point, dist = self.min_dist([self.pose.position.x, self.pose.position.y], self.trajectory.points[0], self.trajectory.points[1])
+        self.min_dist_pub.publish(PointStamped(point=Point(x=min_point[0], y=min_point[1], z=0), header=Header(frame_id="map")))
 
-#     def min_dist(robot_pose, ) {
-#         # Return minimum distance between line segment vw and point p
-#   const float l2 = length_squared(v, w);  // i.e. |w-v|^2 -  avoid a sqrt
-#   if (l2 == 0.0) return distance(p, v);   // v == w case
-#   // Consider the line extending the segment, parameterized as v + t (w - v).
-#   // We find projection of point p onto the line. 
-#   // It falls where t = [(p-v) . (w-v)] / |w-v|^2
-#   // We clamp t from [0,1] to handle points outside the segment vw.
-#   const float t = max(0, min(1, dot(p - v, w - v) / l2));
-#   const vec2 projection = v + t * (w - v);  // Projection falls on the segment
-#   return distance(p, projection);
-# }
+    def min_dist(self, robot_position, v, w):
+        # Return minimum distance between line segment vw and point p
+        # v, w are Point
+        robot_position = np.array(robot_position)
+        w = np.array(w)
+        v = np.array(v)
+
+        l2 = np.linalg.norm(w-v)**2  # i.e. |w-v|^2 -  avoid a sqrt
+        if (l2 == 0.0):
+            return (v, np.linalg.norm(robot_position-v))   # v == w case
+
+        # Consider the line extending the segment, parameterized as v + t (w - v).
+        # We find projection of point p onto the line. 
+        # It falls where t = [(p-v) . (w-v)] / |w-v|^2
+        # We clamp t from [0,1] to handle points outside the segment vw.
+        t = max(0, min(1, np.dot(robot_position - v, w - v) / l2))
+        projection = v + t * (w - v) # Projection falls on the segment
+        return (projection, np.linalg.norm(robot_position - projection))
+
 
 
 if __name__=="__main__":
