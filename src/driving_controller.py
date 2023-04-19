05dfe784038be1a06a26896fda739ea40aaa4bd7
nsthakur <nsthakur@mit.edu>
2023-04-19 16:13:25 -0400

    working better

--- src/driving_controller.py
+++ src/driving_controller.py
@@ -34,11 +34,11 @@ class DrivingController():
         self.prev_time = rospy.get_time()
 
         #working for v=1: p=1, d=.13,
-        #working for v=2: p=1.4, d=.135
-        self.steering_kp = 1
-        self.steering_kd = 0.13
+        #working for v=2: p=1.5, d=.135
+        self.steering_kp = 1.5
+        self.steering_kd = 0.135
 
-        self.velocity_kp = 1
+        self.velocity_kp = 2
         self.velocity_max = 5
 
     def pose_callback(self, odom):
@@ -75,7 +75,7 @@ class DrivingController():
 
         print('y', y_error)
 
-        if y_error > 0.4:
+        if y_error > 0.8:
             velocity = 0
             print('stopping', y_error)
         elif y_error > 0.2:
