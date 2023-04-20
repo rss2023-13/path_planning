9e0a41c7d90536b7b6ec8124b76a487921f76430
SarahZhang25 <szhang025@gmail.com>
2023-04-20 18:42:34 -0400

    added distance capping

--- src/path_planning.py
+++ src/path_planning.py
@@ -21,7 +21,6 @@ class PathPlan(object):
         self.map_resolution = None
         self.current_pose = None
         self.goal_pose = None
-        self.acceptable_goal_cells = None
 
         self.odom_topic = rospy.get_param("~odom_topic")
         self.map_sub = rospy.Subscriber("/map", OccupancyGrid, self.map_cb, queue_size=1)
@@ -72,6 +71,7 @@ class PathPlan(object):
         self.map_height = map_msg.info.height
         self.map_width = map_msg.info.width
         self.map_resolution = map_msg.info.resolution # meters / cell
+        print("resolution:", self.map_resolution)
         # rospy.logerr("map dims: %s %s", self.map_height, self.map_width)
         # self.map_origin = 
 
@@ -85,12 +85,10 @@ class PathPlan(object):
 
         self.trajectory.clear()
         self.parents = {}
-        self.plan_path(self.current_pose, self.goal_pose, self.map, None)
+        self.plan_path(self.current_pose, self.goal_pose, max_distance = 5)
 
     def cell_to_world(self, u, v):
-        '''
-        convert __ to __
-        '''
+        '''convert from map frame to world frame'''
         angle = self.map_origin[2]
         rotation_matrix = np.array([[np.cos(angle), np.sin(angle), 0], 
                                     [-np.sin(angle), np.cos(angle), 0], 
@@ -100,6 +98,7 @@ class PathPlan(object):
         return (rotated_coord[0], rotated_coord[1])
 
     def world_to_cell(self, position):
+        '''convert from world frame to cell'''
         angle = self.map_origin[2]
         rotation_matrix_inv = np.linalg.inv(np.array([[np.cos(angle), np.sin(angle), 0], 
                                                     [-np.sin(angle), np.cos(angle), 0], 
@@ -145,25 +144,39 @@ class PathPlan(object):
         x_orig = (start[0], end[0])
         y_orig = (start[1], end[1])
 
-        dist = np.linalg.norm(np.array((5,5))-np.array((0,0)))
-        step_dist = .3 * self.map_resolution # tune this
+        dist = np.linalg.norm(np.array(start)-np.array(end))
+        step_dist = .25 * self.map_resolution # tune this
         num_pts = int(dist / step_dist) # num pts to interpolate
-        x_interp = np.linspace(x_orig[0], y_orig[0], num_pts)
+        print("checking pts", num_pts)
+        x_interp = np.linspace(x_orig[0], x_orig[1], num_pts)
         y_interp = np.interp(x_interp, x_orig, y_orig)
 
         for i in range(num_pts):
             pt = self.world_to_cell((x_interp[i], y_interp[i])) # get map frame coord
             cell = (int(pt[0]), int(pt[1])) # convert to cell ind
             # print(pt)
-            if cell in checked_cells:
-                continue
-            else:
-                checked_cells.add(cell)
+            # if cell in checked_cells:
+                # continue
+            # else:
+                # checked_cells.add(cell)
             if self.point_collision_check(cell[1], cell[0]):
+                print("path collision")
                 return True
             
         return False
-            
+    
+    def calculate_new_node(self, node_sampled, node_nearest, max_distance):
+        print("calculating new node")
+        node_sampled = np.array(node_sampled)
+        node_nearest = np.array(node_nearest)
+        dist = np.linalg.norm(node_sampled-node_nearest)
+        if dist < max_distance:   
+            return tuple(node_sampled)
+        elif dist >= max_distance:
+            unit_vector = (node_sampled-node_nearest) / dist
+            node_new = node_nearest + unit_vector * max_distance # take point that is max_distance away from node_nearest in direction of node_sampled
+            return tuple(node_new)
+        
 
     def find_nearest_vertex(self, position):
         print("finding nearest vertex")
@@ -171,38 +184,29 @@ class PathPlan(object):
         # simplest heuristic: euclidean distance
         # could consider others like spline? dubins path? -- this can be an optimization task
         # iterate through node list and identify the one with lowest distance
-        dists = np.array([np.linalg.norm(np.array(position) - np.array(v)) for v in self.parents.keys()]) # euclidean distance to all vertices
+        dists = np.array([np.linalg.norm(np.array(position) - np.array(vert)) for vert in self.parents.keys()]) # euclidean distance to all vertices
 
-        sorted_args = np.argsort(dists)
+        # sorted_args = np.argsort(dists)
 
         min_ind = np.argmin(dists)
 
-        existing_nodes = np.array(self.parents.keys())[sorted_args]
+        return self.parents.keys()[min_ind]
 
-        for node in existing_nodes: 
-            if not self.path_collision_check(position, node):
-                return tuple(node)
+        # existing_nodes = np.array(self.parents.keys())[sorted_args]
+
+        # for node in existing_nodes: 
+        #     if not self.path_collision_check(position, node):
+        #         return tuple(node)
         print("no vertices free")
-
-    def reached_goal(self, node):
-        # print("checking if goal reachable")
-        # if can go from node to end w/o intersecting wall
-        # can get rid of this function; replaced with path_collision_check(self, node, end_point) in plan_path()
-        # node_cell = self.world_to_cell(node)
-
-        # print("current node:", node_cell)
-
-        # return node_cell in self.acceptable_goal_cells
-        pass
     
 
     ### rrt alg ###
-    def plan_path(self, start_point, end_point, map, max_distance):
+    def plan_path(self, start_point, end_point, max_distance):
         print("planning path now")
         #TODO Add max_distance parameter, new nodes should not exceed a certain distance from their nearest node
         ## CODE FOR PATH PLANNING ##
         goal_reached = False
-        max_iter = 100
+        max_iter = 250
         current_iter = 0
 
         self.parents[start_point] = None
@@ -212,16 +216,18 @@ class PathPlan(object):
             goal_reached = True 
         
         while not goal_reached and current_iter < max_iter :
-
             print("current iter:", current_iter)
-            node_new = self.sample_map() # point collision check is perfomed in sampling (only return valid samples)
-            node_nearest = self.find_nearest_vertex(node_new)
+            node_sampled = self.sample_map() # point collision check is perfomed in sampling (only return valid samples)
+            node_nearest = self.find_nearest_vertex(node_sampled)
 
+            node_new = self.calculate_new_node(node_sampled, node_nearest, max_distance)
 
-            # update tree
+            if self.path_collision_check(node_new, node_nearest): # discard point if path not feasible
+                continue
+
+            self.parents[node_new] = node_nearest # add to tree
             # self.tree[node_new] = set() # initialize new node in tree
             # self.tree[node_nearest].add(node_new) # add new node to child set of node_nearest
-            self.parents[node_new] = node_nearest
 
             if current_iter == max_iter - 1 or not self.path_collision_check(node_new, end_point):
                 goal_reached = True
@@ -232,6 +238,7 @@ class PathPlan(object):
 
         
         print("path search end, iterations:", current_iter)
+        
         # print("tree", self.parents)
 
         # by this point, the tree has been constructed
@@ -243,13 +250,14 @@ class PathPlan(object):
             current_node = self.parents[current_node] # backtrack to parent node
             reverse_path.append(current_node)
 
+        print("path reconstructed, is", len(reverse_path), "points long")
         for pt in reverse_path[::-1]: # populate trajectory object
             point_obj = Point(x=pt[0], y=pt[1])
             self.trajectory.addPoint(point_obj)
 
         # self.trajectory.addPoint(Point(x=0,y=0))
-        # self.trajectory.addPoint(Point(x=5,y=0))
-        # self.trajectory.addPoint(Point(x=0,y=2))
+        # self.trajectory.addPoint(Point(x=1,y=0))
+        # self.trajectory.addPoint(Point(x=0,y=1))
                     
 
         # publish trajectory
